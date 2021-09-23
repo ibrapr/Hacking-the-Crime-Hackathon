2577db25d0a3c32a15627055dbd9a8b13cfdd987
Ibrahem Chahine <shahena618@gmail.com>
2021-09-23 16:24:40 +0300

    backend commit

---
+++ Backend/src/test/java/com/antibullying/antibullying/AntibullyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antibullying.antibullying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntibullyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
