54972f486c699faa811b97e3320ebd31c3206a3f
Ibrahem Chahine <shahena618@gmail.com>
2021-09-23 16:31:59 +0300

    modified pom,

---
+++ Backend/src/main/java/com/antibullying/antibullying/conf/MailConfiguration.java
@@ -0,0 +1,49 @@
+package com.antibullying.antibullying.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class MailConfiguration {
+	@Autowired
+    private Environment env;
+ 
+//    @Bean
+//    public JavaMailSender getMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+// 
+//        mailSender.setHost(env.getProperty("spring.mail.host"));
+//        mailSender.setPort(Integer.valueOf(env.getProperty("spring.mail.port")));
+//        mailSender.setUsername(env.getProperty("spring.mail.username"));
+//        mailSender.setPassword(env.getProperty("spring.mail.password"));
+// 
+//        Properties javaMailProperties = new Properties();
+//        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+//        javaMailProperties.put("mail.smtp.auth", "true");
+//        javaMailProperties.put("mail.transport.protocol", "smtp");
+//        javaMailProperties.put("mail.debug", "true");
+// 
+//        mailSender.setJavaMailProperties(javaMailProperties);
+//        return mailSender;
+//    }
+    
+    @Bean
+	public Docket api()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+		
+	}
+}
